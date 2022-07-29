e89d1d0cbb7db6e0fddb7636e171290ad6065463
manuelalferez <manuelalferezruiz@gmail.com>
2022-07-29 15:55:27 +0200

    feat: updated mac scripts

--- mac.sh
+++ mac.sh
@@ -1,7 +1,40 @@
+# Development tools
 git --version 
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install --cask iterm2
+
+brew install tmux
+
+brew install node
+
+brew install nvm 
+
+
+# Apps 
+brew install --cask visual-studio-code
+
+brew install --cask spotify
+
+brew install --cask telegram
+
+brew install --cask vlc
+
+brew install --cask the-unarchiver
+
+brew install --cask libreoffice
+
+brew install --cask zoom
+
+brew install --cask rectangle
+
+brew install --cask firefox
+
+brew install --cask slack
+
+brew install --cask alfred
+
+brew install gnupg
